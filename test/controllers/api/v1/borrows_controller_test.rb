eb9c4901005931ac22201e2393c7c66f62cba8f9
iewad <lidawei.me@gmail.com>
2020-07-16 17:51:45 +0800

    Add test borrow books

--- test/controllers/api/v1/borrows_controller_test.rb
+++ test/controllers/api/v1/borrows_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class Api::V1::BorrowsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "borrow book should not work" do
+    user = users(:one)
+    books = books(:one, :two, :three)
+
+    post "/api/v1/borrows",
+      params: {user_no: user.user_no, isbns: books.pluck(:isbn)}
+
+    assert_response :error
+  end
+
+  test "borrow book should work" do
+    user = users(:one)
+    books = books(:one, :two)
+
+    post "/api/v1/borrows",
+      params: {user_no: user.user_no, isbns: books.pluck(:isbn)}
+
+    assert_response :success
+  end
 end
