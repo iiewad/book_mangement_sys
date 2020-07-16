014714c947c06a46386f4697369aeea311ebd492
iewad <lidawei.me@gmail.com>
2020-07-16 17:55:18 +0800

    Add test for book show

--- test/controllers/api/v1/books_controller_test.rb
+++ test/controllers/api/v1/books_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class Api::V1::BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "show book should work" do
+    book = books(:one)
+    get "/api/v1/books/#{book.id}"
+
+    assert_response :success
+  end
 end
