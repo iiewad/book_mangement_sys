a071ace2110a8e3183e678e159cf0d22d3964c30
Thomas Lee <thomas.li@anker.io>
2020-08-07 11:29:56 +0800

    user borrow info query optimization

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < Api::BaseController
     user = User.find_by(user_no: params[:user_no])
     data = {
       balance: user.account&.balance,
-      borrow_books: user.borrows.map(&:as_json)
+      borrow_books: user.includes(:borrows).map(&:as_json)
     }
     return render json: {code: 0, data: data, msg: "ok"}
   end
