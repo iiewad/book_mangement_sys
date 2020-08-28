6883407ef8c54909506b262d6f3d865c03b67886
Thomas Lee <thomas.li@anker.io>
2020-08-28 12:03:44 +0800

    add gem seed_dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,66 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  user = User.create(email: Faker::Internet.email,
-                     password: "password")
-
-  Account.create(user_id: user.id, balance: 1000.to_d)
-
-  Book.create(isbn: Faker::Code.isbn,
-              title: Faker::Book.title,
-              author: Faker::Book.author,
-              publisher: Faker::Book.publisher,
-              publish_date: Faker::Date.birthday(min_age: 18, max_age: 65),
-              borrow_price: Faker::Commerce.price.to_d,
-              stock: rand(0..10))
-end
-
+User.create!([
+  {email: "stephan.homenick@rohan.io", encrypted_password: "$2a$12$vY54cYoH7jL3W9jDLlN4/.QaXgNKb7m6koKsjZFU01qMXQGQPg4KS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "a6f1ffd64a2fcfb56b71"},
+  {email: "karen@hammes.io", encrypted_password: "$2a$12$p7QpFydu3Aklj59eDksqmuIBHwndj5UiAuzRLynO99SH7y7bqTWQ2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "e62b1fab9a6f95c4efe0"},
+  {email: "jere@nolan-oreilly.com", encrypted_password: "$2a$12$iH5Dwqz1Abz56gGoOU5QA.rgzuYqsVIyxC74bYZDy1zGXdwTiCkcO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "048232fc81e56acaa938"},
+  {email: "carter@watsica.org", encrypted_password: "$2a$12$l6aPN0ADQebTNVFyASytUutbQEiN97ROf/nFIOgPSjJBecDsVu92K", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "03f9ebb4697ff8b24d58"},
+  {email: "charissa_pouros@berge-durgan.net", encrypted_password: "$2a$12$QLKvz6q7k8khsbvvohT/R.Y2YxfHHqg9K0Ovb7fN1jsQ1YAIgSBMu", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "f66dcf2d1ca321850d5c"},
+  {email: "karie_rodriguez@ryan-nicolas.org", encrypted_password: "$2a$12$gXlqsAhgDXKY0W1tf3L2x.zdbbzA8Rm8M0CRtKEWUDFbQE5emKqIy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "28fc275069d1a3554f27"},
+  {email: "lauralee_block@bartell.biz", encrypted_password: "$2a$12$DmWI2kYkF5ydPBpP99gZq.G//m5Cb6.f6cemyElytl6P5e12eXYB2", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "604c8699170a4110ca06"},
+  {email: "irena@hilll-schultz.info", encrypted_password: "$2a$12$4xtSPensNofHYqFTCknYPuiEcD9htiyiq45zJ1.46Zfr4op0J8uOK", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "1bf3ec31a57668a887fa"},
+  {email: "marco.ferry@okuneva.biz", encrypted_password: "$2a$12$x7M3PqXcvpSfiqv76OohqOzMR3zAAn94Kche49iTNXvrCugWUM0wC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "67ea7c92343090fbe874"},
+  {email: "jere.waelchi@von.biz", encrypted_password: "$2a$12$C4JA.eaeG6CxE9ERt2eF2uMsiJBvqKyWGqlFp4xzJc.IsvA5/TkIO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "fd2e72586f2e3fea9282"},
+  {email: "sam@hegmann.name", encrypted_password: "$2a$12$F1pXAYP.wE.uRBeiybsL1.A7ZtVmEmiPjY6OjEs7CaAbr/WW1mzjO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "ff1f04da42b59aa01b2f"},
+  {email: "france_wiza@walter.info", encrypted_password: "$2a$12$jbNOBaDp5Zl5jHqB0zjsNOHi1uYxF4KTJHBTR4wVQ8tfoB15fQ/OO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "ce03abb64276740fa407"},
+  {email: "numbers.macejkovic@king.name", encrypted_password: "$2a$12$r.S8ET9ihHFDkgVb/eAOveU7a/gphwK7KS/FNM2PlLwYIYbZVnFJy", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "378846a00b7b4533d7d1"},
+  {email: "coretta@beatty.biz", encrypted_password: "$2a$12$.4G9r.BdisxhN9zhWyuwLO07HZ7dfLuxUUSW6IaIwYp9PxSEZJixm", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "d442a22a5e4a8982463d"},
+  {email: "joshua@huel.org", encrypted_password: "$2a$12$9TPuxmvVBG8gSGbcZmoTzujdqom3UCKcIB6xNmJ435Zdzyh7E8lGq", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "70d62996d107a80b01ef"},
+  {email: "chang@runte.org", encrypted_password: "$2a$12$P3AYBgOXrBEmqVcIn176mucj6GjVMNFkNkIk7OtZjpJTqPGHQHC5a", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "e4f2abba3cc9aeb1491b"},
+  {email: "coleman_ullrich@haag.com", encrypted_password: "$2a$12$jlh58QaXbP8xGIo6hb1sEeOnInj7Hm.Y/Uu0fWRNVukoDFPJ5LGFS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "5936fd2ea6ecb0594cde"},
+  {email: "rudolf_tremblay@armstrong-konopelski.net", encrypted_password: "$2a$12$DADqoVazPjYdeqFLGJTTOOgzzmLGJYMoZbyE53tTTzT/LcE28WCR.", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "3e8122dd258f2e35e669"},
+  {email: "evan.effertz@grant-dooley.info", encrypted_password: "$2a$12$2j8/yJCuHgHvdxGOBOg7VOwtzWzr6aH43AjWyUtXpQ.kpm0qPI9kW", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "109504b6ada3de7c7725"},
+  {email: "sherly_stehr@kris.com", encrypted_password: "$2a$12$v9qsUKh4.3iPWIMHP1vee.AmjDIiyj61eh5A/5JjHrQrHNbnhXJCS", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_no: "b32a7244a797812a7f65"}
+])
+Account.create!([
+  {user_id: 1, balance: "712.7"},
+  {user_id: 2, balance: "1000.0"},
+  {user_id: 3, balance: "1000.0"},
+  {user_id: 4, balance: "1000.0"},
+  {user_id: 5, balance: "1000.0"},
+  {user_id: 6, balance: "1000.0"},
+  {user_id: 7, balance: "1000.0"},
+  {user_id: 8, balance: "1000.0"},
+  {user_id: 9, balance: "1000.0"},
+  {user_id: 10, balance: "1000.0"},
+  {user_id: 11, balance: "1000.0"},
+  {user_id: 12, balance: "1000.0"},
+  {user_id: 13, balance: "1000.0"},
+  {user_id: 14, balance: "1000.0"},
+  {user_id: 15, balance: "1000.0"},
+  {user_id: 16, balance: "1000.0"},
+  {user_id: 17, balance: "1000.0"},
+  {user_id: 18, balance: "1000.0"},
+  {user_id: 19, balance: "1000.0"},
+  {user_id: 20, balance: "1000.0"}
+])
+Book.create!([
+  {isbn: "199365067-9", author: "Jani Kozey", publisher: "Faber and Faber", intro: nil, remark: nil, publish_date: "1977-05-24 00:00:00", title: "Nectar in a Sieve", borrow_price: "47.55", stock: 0},
+  {isbn: "427325272-4", author: "Ed Morar", publisher: "Haynes Manuals", intro: nil, remark: nil, publish_date: "1999-05-18 00:00:00", title: "The Way of All Flesh", borrow_price: "10.94", stock: 6},
+  {isbn: "563103005-5", author: "Sharyn Barrows", publisher: "Brill Publishers", intro: nil, remark: nil, publish_date: "1984-08-03 00:00:00", title: "Quo Vadis", borrow_price: "93.57", stock: 7},
+  {isbn: "131705312-5", author: "Carlton Mann", publisher: "G-Unit Books", intro: nil, remark: nil, publish_date: "1998-04-11 00:00:00", title: "Blithe Spirit", borrow_price: "46.05", stock: 4},
+  {isbn: "131069199-1", author: "Sharda Towne", publisher: "Mandrake of Oxford", intro: nil, remark: nil, publish_date: "1992-11-26 00:00:00", title: "The Line of Beauty", borrow_price: "73.07", stock: 2},
+  {isbn: "757949543-0", author: "Boyce Bode Jr.", publisher: "Blackstaff Press", intro: nil, remark: nil, publish_date: "1998-01-25 00:00:00", title: "Infinite Jest", borrow_price: "63.67", stock: 9},
+  {isbn: "832833012-1", author: "Leanne Koelpin", publisher: "New Holland Publishers", intro: nil, remark: nil, publish_date: "1955-08-28 00:00:00", title: "Ring of Bright Water", borrow_price: "48.55", stock: 4},
+  {isbn: "053129030-1", author: "Merrill Bechtelar", publisher: "Cloverdale Corporation", intro: nil, remark: nil, publish_date: "1960-08-25 00:00:00", title: "A Passage to India", borrow_price: "44.72", stock: 2},
+  {isbn: "310679288-4", author: "Wilton King MD", publisher: "HMSO", intro: nil, remark: nil, publish_date: "1961-08-16 00:00:00", title: "Look Homeward, Angel", borrow_price: "9.36", stock: 2},
+  {isbn: "116852071-1", author: "Alexander Lind", publisher: "HarperCollins", intro: nil, remark: nil, publish_date: "1960-06-12 00:00:00", title: "A Summer Bird-Cage", borrow_price: "69.97", stock: 1}
+])
+Borrow.create!([
+  {isbn: "427325272-4", user_id: 1, borrow_date: "2020-08-07 03:26:21", return_date: "2020-09-07 03:26:21", remark: nil, book_id: 2},
+  {isbn: "563103005-5", user_id: 1, borrow_date: "2020-08-07 03:26:21", return_date: "2020-09-07 03:26:21", remark: nil, book_id: 3},
+  {isbn: "131705312-5", user_id: 1, borrow_date: "2020-08-07 03:26:21", return_date: "2020-09-07 03:26:21", remark: nil, book_id: 4},
+  {isbn: "131069199-1", user_id: 1, borrow_date: "2020-08-07 03:26:21", return_date: "2020-09-07 03:26:21", remark: nil, book_id: 5},
+  {isbn: "757949543-0", user_id: 1, borrow_date: "2020-08-07 03:26:21", return_date: "2020-09-07 03:26:21", remark: nil, book_id: 6}
+])
